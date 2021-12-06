19d4f25048879ea8f9a69685a9301e37df724b55
Eemi Kärkkäinen <eemi.karkkainen@aalto.fi>
2021-12-06 21:03:59 +0200

    Efect epilepse

--- src/main/scala/s1/demo/Epilepse.scala
+++ src/main/scala/s1/demo/Epilepse.scala
@@ -7,33 +7,40 @@ import scala.util.Random
 /**
  * This simple effect features a bouncing ball
  */
-object BouncingBall extends Effect(500, 500){
-  
+object Epilepse extends Effect(500, 500){
+
   // The ball has coordinates and speed
-  class Ball(val x: Int, val y:Int, val xSpeed: Int, val ySpeed: Int)
- 
+  class Ball(val x: Int, val y:Int, val width: Int)
+  class Recta
+
+
+  var pic      = emptyImage
   // ...and here is our initial instance
-  var ball = new Ball(200, 200, 10, 8)
+  var ball = new Ball(100, 100, 400)
   
   val random = new Random
-  var clock  = 0 
+  var clock  = 0
   
   /**
    * Here we draw a BufferedImage on the current state of the [[Effect]]
    */
+  var speed = 0.0
+
   def makePic() = {
     // Get an empty space where to draw 
-    val pic      = emptyImage
+
     
     // Get the tools to draw with
     val graphics = pic.graphics
 
     // We can pick a random color
-    val randomColor = new java.awt.Color(random.nextInt(256),random.nextInt(256),40 + random.nextInt(216), 255)
 
     // And then use it to fill an oval
-    graphics.setColor(randomColor)
-    graphics.fillOval(ball.x, ball.y, 40, 40)
+    if (clock % 2 == 0)
+    graphics.setColor(java.awt.Color.WHITE)
+    else graphics.setColor(java.awt.Color.BLACK)
+    if (clock % 2 == 0) graphics.fillOval(100, 100, ball.width, ball.width)
+    else graphics.fillRect(100, 100, ball.width, ball.width)
       
     // Finally we return the picture we created.
     pic
@@ -43,9 +50,24 @@ object BouncingBall extends Effect(500, 500){
    * Here we modify the state (the position and speed of the ball)
    */
   def changeThings() = {
-    clock += 1 
+    clock += 1
+
+
+    var newRadius = 200
+
+    if (clock < 100) {
+      speed += 0.1
+      newRadius = ball.width - (speed).toInt
+
+    } else {
+      speed += 0.1
+      newRadius = ball.width + (speed + 3.0).toInt
+
+    }
+
+
     
-    val y = ball.y + ball.ySpeed
+   /* val y = ball.y + ball.ySpeed
     val x = ball.x + ball.xSpeed
     
     val (nextY, nextYSpeed) = 
@@ -62,14 +84,15 @@ object BouncingBall extends Effect(500, 500){
       } else if (x < 0)
         (-x, -ball.xSpeed)
       else
-        (x, ball.xSpeed)
+        (x, ball.xSpeed) */
     
     // We could have done this with a ball with it's coordinates in var's
     // It can also be done in a more functional way, replacing the ball
     // itself
-    ball = new Ball(nextX, nextY, nextXSpeed, nextYSpeed)
+    ball = new Ball(100, 100, newRadius)
     
   }
+
   
   def next = clock > 300
 }
--- src/main/scala/s1/demo/Snakes.scala
+++ src/main/scala/s1/demo/Snakes.scala
@@ -1,109 +1,104 @@
 package s1.demo
 
 import java.awt.image.BufferedImage
-import java.awt.BasicStroke
 import s1.image.ImageExtensions._
-import scala.collection.mutable.Queue
-import java.awt.Font
-import java.awt.Color._
 
 class Circle(val x: Int, val y: Int)
 
 /**
  * The idea for this effect came from Felix Bade.
- * 
+ *
  * The effect draws a continuous stream of filled
  * circles that changes it's course randomly.
  */
+
+
 object Snakes extends Effect(500, 500) {
-  
+
     // This variable could hold a background image if wanted
-    //  See [[DIssolve]] for an exampleon how to load image files
-    val background: Option[BufferedImage] = None      
-    
-    // The circles we draw are in a queue.The latest is
-    // always stored in [[last]]
-    var last        = new Circle(100, 100)
-    val circles     = Queue[Circle](last)
-    var direction   = 0.0 
-    val step        = 10
-    var queueLength = 1
-    
+    // See [[DIssolve]] for an exampleon how to load image files
+    var clock = 0
+
     val random = new util.Random
-    
+
     def changeThings() = {
-      // Change the direction where to draw the next circle randomly
-      direction = direction + (random.nextDouble - 0.5);
-      
-      // Calculate the new coordinates 
-      val xdiff = (math.cos(direction) * step).toInt
-      val ydiff = (math.sin(direction) * step).toInt
-      
-      val x = (last.x + xdiff + 500) % 500
-      val y = (last.y + ydiff + 500) % 500
-      last  = new Circle(x,y)
-      
-      // Store the circle in a queue (Think of a buffer where you add in one end and take stuff out from the other)
-      circles.enqueue( last )
-      queueLength += 1
-      
-      // If the queue gets big, we delete older circles for a fun effect
-      while (queueLength > 600) {
-        circles.dequeue()
-        queueLength -= 1
-      }
+
+      clock += 1
+
     }
-    
+
     //------- drawing -------//
-    
+
     // Thick and thin line widths
-    val thick = new BasicStroke(3)
-    val thin  = new BasicStroke(1)
-    
-    // Nice font for the christmas message
-    val christmasFont  = new Font("Times", Font.BOLD, 60)
-    
     override def makePic(): BufferedImage = {
       val pic = emptyImage
-      val g = pic.graphics
-      
-      // If there was a background we could paint it here
-      // The parameters of drawImage are picture, x, y and an ImageObserver which we in this case leave null.
-      background foreach (image => g.drawImage(image, 0, 0, null))
-      
-      // Draw all the circles.
-      
-      // For more ideas on drawing see:
-      //   https://docs.oracle.com/javase/7/docs/api/java/awt/Graphics.html
-      //   https://docs.oracle.com/javase/7/docs/api/java/awt/Graphics2D.html
-      //   https://docs.oracle.com/javase/tutorial/2d/basic2d/index.html   
-      for (c <- circles) {
-        g.setColor(WHITE)
-        g.setStroke(thin)
-        g.fillOval(c.x - 20, c.y - 20, 40, 40)
-        
-        g.setStroke(thick)
-        g.setColor(BLACK)
-        g.drawOval(c.x - 20, c.y - 20, 40, 40)
+      val graphics = pic.graphics
+
+      if(clock%4 == 1){
+      graphics.setColor(java.awt.Color.black)
+      } else if(clock%4 ==2) {
+      graphics.setColor(java.awt.Color.black)
+      } else if(clock%4 ==3) {
+      graphics.setColor(java.awt.Color.black)
+      } else graphics.setColor(java.awt.Color.black)
+
+      graphics.fillRect(0, 0, 500, 500)
+
+      for (i <- 1 to 30) {
+
+        val radius = 700 - i * 10
+        val xCenter = 250
+        val yCenter = 250
+
+        var corners = 7
+
+        if (clock > 100) {
+          corners = 6
+        }
+        if (clock > 200) {
+          corners = 5
+        }
+        if (clock > 300) {
+          corners = 4
+        }
+        if (clock > 400) {
+          corners = 3
+        }
+
+        val cornersFloat = corners * 2.0
+
+        var speed = 0.1
+
+        if(clock > 100){
+          speed = -0.1
+        }
+
+        var circle = i * 10
+
+        val xs = Array.tabulate(corners)( x => ( radius * math.cos(clock * i * speed + x / cornersFloat * (2.0 * math.Pi))).toInt + xCenter)
+        val ys = Array.tabulate(corners)( x => ( radius * math.sin(clock * i * speed + x / cornersFloat * (2.0 * math.Pi))).toInt + yCenter)
+        val hs = corners
+        println(xs.mkString("Array(", ", ", ")"))
+        if(i% 2 == 1) {
+          graphics.setColor(java.awt.Color.black)
+        } else {
+          graphics.setColor(java.awt.Color.white)
+        }
+        //graphics.fillPolygon
+        //graphics.fillRect(250, 250, 100, 100)
+        //graphics.setColor(java.awt.Color.pink)
+        graphics.fillOval(150 - circle, 150 - circle, 500 - circle, 500 - circle)
       }
-      g.setFont(christmasFont)
-
-      // Notice the fake "shadow" under the text
-      g.setColor(BLACK)
-      g.drawString("Hyvää joulua!", 22, 42)
-
-      g.setColor(GREEN)
-      g.drawString("Hyvää joulua!", 20, 40)
-      
       pic
     }
-    
+
     // Effects can also receive information on mouse movements.
     // When a mouse goes to ne coordinates this method gets called.
-    
+
     // We use it to draw still more circles at the mouse location
-    override def mouseAt(x: Int, y: Int) = circles enqueue new Circle(x,y)        
- 
+
     // This effect will never end
-    def next = false
+    def next = clock > 100
   }
+
+
